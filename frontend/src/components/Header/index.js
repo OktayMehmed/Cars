52edaef82f8b297d9193e86a6ce208647f283157
Oktay Mehmed <oktay888@abv.bg>
2021-11-15 22:32:07 +0200

    Add Navigation

--- frontend/src/components/Header/index.js
+++ frontend/src/components/Header/index.js
@@ -1,15 +1,28 @@
-import React from "react";
-import './styles.css';
+import React, { useState } from "react";
+import "./styles.css";
 
 const Header = () => {
+  const [showNav, setShowNav] = useState(false)
+
   return (
-  <header>
-    <article className="header-article">
-    <button className="header-article-menu"><i className="fas fa-bars"></i></button>
-    <h1 className='header-article-logo'>CARS.com</h1>
-    </article>
-  </header>
-  )
+    <>
+      <header>
+        <article className="header-article">
+          <nav className='header-navigation' style={{ width: showNav ? 250 : 0 }}>
+          <h1>Menu</h1>
+          <a href="#">Home</a>
+          <a href="#">Login</a>
+          <a href="#"><i class="fas fa-plus"></i> Post a car</a>
+          <button className="header-navigation-closeBtn" onClick={() => setShowNav(false)}>&times;</button>
+          </nav>
+          <button className="header-article-menu" onClick={() => setShowNav(true)}>
+            <i className="fas fa-bars"></i>
+          </button>
+          <h1 className="header-article-logo">CARS.com</h1>
+        </article>
+      </header>
+    </>
+  );
 };
 
 export default Header;
