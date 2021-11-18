bd01be3f43eaeccf8e2cca3606c005b0049fa3b8
Oktay Mehmed <oktay888@abv.bg>
2021-11-18 20:47:14 +0200

    GET cars from backend

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cars = require("./data/cars");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+app.get("/api/cars", (req, res) => {
+  res.json(cars);
+});
+
+app.get("/api/cars/:id", (req, res) => {
+  const car = cars.find((c) => c._id === req.params.id);
+  res.json(car);
+});
+
+app.listen(8000, console.log("Server is running on port 8000!"));
