542c764db1efdb2a44132bd1ea254f0b5cd846f3
Oktay Mehmed <oktay888@abv.bg>
2021-12-01 23:09:05 +0200

    Show User in Navbar and Logout

--- frontend/src/components/Header/index.js
+++ frontend/src/components/Header/index.js
@@ -1,11 +1,22 @@
 import React, { useState } from "react";
 import { Link, useLocation } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { logout } from "../../actions/User";
 import "./styles.css";
 
 const Header = ({ carId }) => {
   const [showNav, setShowNav] = useState(false);
 
+  const dispatch = useDispatch();
+
+  const userLogin = useSelector((state) => state.userLogin);
+  const { userInfo } = userLogin;
+
   const { pathname } = useLocation();
+
+  const logoutHandler = () => {
+    dispatch(logout());
+  };
 
   return (
     <>
@@ -15,20 +26,32 @@ const Header = ({ carId }) => {
             className="header-navigation"
             style={{ width: showNav ? 250 : 0 }}
           >
-            <h1 className='header-menu'>Menu</h1>
+            <h1 className="header-menu">Menu</h1>
+            {userInfo && (
+              <p className="header-username">Hello, {userInfo.name}</p>
+            )}
             <Link to="/">Home</Link>
-            <Link to="/login">
-              <i className="fas fa-sign-in-alt"></i> Login
-            </Link>
-            <Link to="/profile">
-              <i className="fas fa-user"></i> Profile
-            </Link>
-            <Link to="/post-car">
-              <i className="fas fa-plus"></i> Post a car
-            </Link>
-            <Link to="/my-cars">
-              <i className="fas fa-car"></i> My Cars
-            </Link>
+            {userInfo ? (
+              <>
+                <Link to="/" onClick={logoutHandler}>
+                  <i className="fas fa-sign-out-alt"></i> Logout
+                </Link>
+                <Link to="/profile">
+                  <i className="fas fa-user"></i> Profile
+                </Link>
+                <Link to="/post-car">
+                  <i className="fas fa-plus"></i> Post a car
+                </Link>
+                <Link to="/my-cars">
+                  <i className="fas fa-car"></i> My Cars
+                </Link>
+              </>
+            ) : (
+              <Link to="/login">
+                <i className="fas fa-sign-in-alt"></i> Login
+              </Link>
+            )}
+
             <button
               className="header-navigation-closeBtn"
               onClick={() => setShowNav(false)}
